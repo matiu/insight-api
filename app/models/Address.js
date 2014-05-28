12bb1c375cb1fbb24163f0a30ae43987e4d85e20
Matias Alejo Garcia <ematiu@gmail.com>
2014-05-28 09:31:59 -0300

    refactor addr update opts

--- app/models/Address.js
+++ app/models/Address.js
@@ -74,56 +74,25 @@ function Address(addrStr) {
 
 }
 
-Address.prototype.getUtxo = function(next) {
-  var self = this;
-  var tDb   = TransactionDb;
-  var bDb   = BlockDb;
-  var ret;
-  if (!self.addrStr) return next(new Error('no error'));
 
-  tDb.fromAddr(self.addrStr, function(err,txOut){
-    if (err) return next(err);
-    var unspent = txOut.filter(function(x){
-      return !x.spentTxId;
-    });
-
-    bDb.fillConfirmations(unspent, function() {
-      tDb.fillScriptPubKey(unspent, function() {
-        ret = unspent.map(function(x){
-          return {
-            address: self.addrStr,
-            txid: x.txid,
-            vout: x.index,
-            ts: x.ts,
-            scriptPubKey: x.scriptPubKey,
-            amount: x.value_sat / BitcoreUtil.COIN,
-            confirmations: x.isConfirmedCached ? (config.safeConfirmations+'+') : x.confirmations,
-          };
-        });
-        return next(null, ret);
-      });
-    });
-  });
-};
-
-
-Address.prototype._addTxItem = function(txItem, notxlist) {
+Address.prototype._addTxItem = function(txItem, txList) {
   var add=0, addSpend=0;
   var v = txItem.value_sat;
   var seen = this.seen;
-  var txs = [];
 
+  // Founding tx
   if ( !seen[txItem.txid] ) {
-    if (!notxlist) {
-      txs.push({txid: txItem.txid, ts: txItem.ts});
-    }
     seen[txItem.txid]=1;
     add=1;
+
+    if (txList)
+      txList.push({txid: txItem.txid, ts: txItem.ts});
   }
 
+  // Spent tx
   if (txItem.spentTxId && !seen[txItem.spentTxId]  ) {
-    if (!notxlist) {
-      txs.push({txid: txItem.spentTxId, ts: txItem.spentTs});
+    if (txList) {
+      txList.push({txid: txItem.spentTxId, ts: txItem.spentTs});
     }
     seen[txItem.spentTxId]=1;
     addSpend=1;
@@ -151,8 +120,6 @@ Address.prototype._addTxItem = function(txItem, notxlist) {
     this.unconfirmedBalanceSat += v;
     this.unconfirmedTxApperances += add;
   }
-
-  return txs;
 };
 
 Address.prototype._setTxs = function(txs) {
@@ -168,11 +135,12 @@ Address.prototype._setTxs = function(txs) {
   this.transactions = txs.map(function(i) { return i.txid; } );
 };
 
-Address.prototype.update = function(next, notxlist) {
+Address.prototype.update = function(next, opts) {
   var self = this;
   if (!self.addrStr) return next();
+  opts = opts || {};
 
-  var txs  = [];
+  var txList  = opts.noTxList ? null : [];
   var tDb   = TransactionDb;
   var bDb   = BlockDb;
   tDb.fromAddr(self.addrStr, function(err,txOut){
@@ -184,12 +152,44 @@ Address.prototype.update = function(next, notxlist) {
         if (err) return next(err);
 
         txOut.forEach(function(txItem){
-          txs=txs.concat(self._addTxItem(txItem, notxlist));
+          self._addTxItem(txItem, txList);
         });
 
-        if (!notxlist)
-          self._setTxs(txs);
+        if (txList)
+          self._setTxs(txList);
         return next();
+      });
+    });
+  });
+};
+
+Address.prototype.getUtxo = function(next) {
+  var self = this;
+  var tDb   = TransactionDb;
+  var bDb   = BlockDb;
+  var ret;
+  if (!self.addrStr) return next(new Error('no error'));
+
+  tDb.fromAddr(self.addrStr, function(err,txOut){
+    if (err) return next(err);
+    var unspent = txOut.filter(function(x){
+      return !x.spentTxId;
+    });
+
+    bDb.fillConfirmations(unspent, function() {
+      tDb.fillScriptPubKey(unspent, function() {
+        ret = unspent.map(function(x){
+          return {
+            address: self.addrStr,
+            txid: x.txid,
+            vout: x.index,
+            ts: x.ts,
+            scriptPubKey: x.scriptPubKey,
+            amount: x.value_sat / BitcoreUtil.COIN,
+            confirmations: x.isConfirmedCached ? (config.safeConfirmations+'+') : x.confirmations,
+          };
+        });
+        return next(null, ret);
       });
     });
   });
